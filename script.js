d1fa9b70451378eb9d09e9c64d573ac956d549cd
RafayMemon01 <rafayrashid200611@gmail.com>
2025-02-03 02:13:05 +0500

    some changes for better response

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 window.onload = ()=>{
     setTimeout(()=>{
         document.querySelector(".spinnerContainer").style.display = "none";
@@ -28,3 +30,46 @@ document.addEventListener("DOMContentLoaded", function () {
     elements.forEach(element => observer.observe(element));
 });
 
+let different = ["Business", "Karobar"];
+
+let index = 0; 
+let heroTextElement = document.getElementById("heroText");
+
+function typeText(text, callback) {
+    let currentIndex = 0;
+    heroTextElement.innerHTML = ""; 
+    let typingInterval = setInterval(function() {
+        heroTextElement.innerHTML += text[currentIndex];
+        currentIndex++;
+        if (currentIndex === text.length) {
+            clearInterval(typingInterval);
+            setTimeout(callback, 2000); 
+        }
+    }, 100); 
+}
+
+function changeText() {
+    typeText(different[index], function() {
+        index = (index + 1) % different.length; 
+        changeText(); 
+    });
+}
+
+changeText();
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".youtube-container").forEach(function (el) {
+        el.addEventListener("click", function () {
+            let videoId = this.getAttribute("data-video-id");
+            let iframe = document.createElement("iframe");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + videoId + "?autoplay=1&rel=0");
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+            iframe.setAttribute("allowfullscreen", "true");
+            iframe.setAttribute("width", "100%");
+            iframe.setAttribute("height", "200");
+            this.innerHTML = "";
+            this.appendChild(iframe);
+        });
+    });
+});
